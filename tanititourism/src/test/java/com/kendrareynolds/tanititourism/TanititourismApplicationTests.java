edb055f514e5c2bf6aa23499b72dd53d131ffaad
Kendra Reynolds <krey214@wgu.edu>
2023-06-03 19:56:30 -0700

    Initial commit with Spring Boot application

---
+++ tanititourism/src/test/java/com/kendrareynolds/tanititourism/TanititourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kendrareynolds.tanititourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanititourismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
